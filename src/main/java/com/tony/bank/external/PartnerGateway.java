491bee0d3c2d14bbb420a04f3fe617517307e0ea
Tung-Xie <tony.xie@chelpis.com>
2026-02-13 15:16:57 +0800

    Update PartnerGateway.java

--- src/main/java/com/tony/bank/external/PartnerGateway.java
+++ src/main/java/com/tony/bank/external/PartnerGateway.java
@@ -1,12 +1,12 @@
 package com.tony.bank.external;
 import java.security.*;
-import java.security.spec.ECGenParameterSpec;
+import java.security.spec.*;
+
 public class PartnerGateway {
     public void connect() throws Exception {
-        // 弱橢圓曲線 (Weak Curves)
-        KeyPairGenerator ec = KeyPairGenerator.getInstance("EC");
-        ec.initialize(new ECGenParameterSpec("secp160k1"));
-        ec.initialize(new ECGenParameterSpec("sect163k1"));
-        ec.initialize(new ECGenParameterSpec("secp192r1"));
+        KeyPairGenerator ecGen = KeyPairGenerator.getInstance("EC");
+        // 顯式字串最容易被掃描器捕捉
+        ecGen.initialize(new ECGenParameterSpec("secp160k1"));
+        ecGen.initialize(new ECGenParameterSpec("secp192r1"));
     }
 }
