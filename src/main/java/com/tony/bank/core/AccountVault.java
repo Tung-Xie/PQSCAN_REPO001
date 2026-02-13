2a08476c7ccefafe630e98ede01943324dd766be
Tung-Xie <tony.xie@chelpis.com>
2026-02-13 15:15:43 +0800

    Update AccountVault.java

--- src/main/java/com/tony/bank/core/AccountVault.java
+++ src/main/java/com/tony/bank/core/AccountVault.java
@@ -1,13 +1,17 @@
 package com.tony.bank.core;
 import java.security.*;
 import javax.crypto.*;
+
 public class AccountVault {
     public void encryptData() throws Exception {
-        // PKI: 過時公鑰標準
-        KeyPairGenerator rsa = KeyPairGenerator.getInstance("RSA");
-        rsa.initialize(1024);
-        // 不安全的對稱加密模式
-        Cipher aesEcb = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        // 測試 RSA 金鑰長度識別
+        KeyPairGenerator rsaGen = KeyPairGenerator.getInstance("RSA");
+        rsaGen.initialize(1024); 
+        
+        // 測試不安全模式識別
+        Cipher aes = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        
+        // 測試極弱對稱加密
         Cipher des = Cipher.getInstance("DES/CBC/PKCS5Padding");
     }
 }
