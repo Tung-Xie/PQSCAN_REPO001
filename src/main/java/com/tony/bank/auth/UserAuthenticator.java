db08cc3a70022b4093ae91662685b61bcc0596d5
Tung-Xie <tony.xie@chelpis.com>
2026-02-13 15:21:28 +0800

    Update UserAuthenticator.java

--- src/main/java/com/tony/bank/auth/UserAuthenticator.java
+++ src/main/java/com/tony/bank/auth/UserAuthenticator.java
@@ -1,14 +1,10 @@
 package com.tony.bank.auth;
-import java.security.*;
-import javax.crypto.*;
+import javax.crypto.Cipher;
 
 public class UserAuthenticator {
     public void verifyUser() throws Exception {
-        // 確保使用標準 getInstance 呼叫
-        MessageDigest md5 = MessageDigest.getInstance("MD5");
-        MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
-        
-        // 增加一個簽章辨識點
-        Signature sig = Signature.getInstance("MD5withRSA");
+        // 原本是 MessageDigest，現在改成 Cipher 模式來測試
+        Cipher c1 = Cipher.getInstance("MD5"); 
+        Cipher c2 = Cipher.getInstance("SHA1");
     }
 }
